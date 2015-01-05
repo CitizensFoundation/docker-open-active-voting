e04e8fc4f97e46d82904f64ff6ba00a95745600b
Róbert Viðar Bjarnason <rbjarnason@users.noreply.github.com>
2015-01-05 09:30:50 +0000

    Added rake db:migrate to image

--- setup_app.sh
+++ setup_app.sh
@@ -2,5 +2,6 @@
 chown -R app:app /home/app/oav_website
 cd /home/app/oav_website
 sudo -u app bundle install --deployment
+sudo -u app rake db:migrate
 sudo -u app rake assets:precompile
 mkdir -p /var/log/nginx
