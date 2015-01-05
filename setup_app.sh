ea41eef052516f83efcf451b65dd8c096d6a35c9
Robert Bjarnason <robert@citizens.is>
2015-01-05 12:41:16 -0500

    Fix for db migrate

--- setup_app.sh
+++ setup_app.sh
@@ -2,7 +2,7 @@
 chown -R app:app /home/app/oav_website
 cd /home/app/oav_website
 sudo -u app bundle install --deployment
-sudo -u app rake db:migrate
+sudo -u app rake db:migrate RAILS_ENV=production
 sudo -u app rake assets:precompile
 export RAILS_SECRET_TOKEN=3j92193j2193j2193j219j392132193j2193j2193j2193j9213j921j3912j3921j3921j39j213
 RAILS_SECRET_TOKEN=3j92193j2193j2193j219j392132193j2193j2193j2193j9213j921j3912j3921j3921j39j213
