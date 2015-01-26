51a1b15473fe6cbcf6dfdc578b49deb14c5543b2
Róbert Viðar Bjarnason <rbjarnason@users.noreply.github.com>
2015-01-26 22:23:25 +0000

    Update setup_app.sh

--- setup_app.sh
+++ setup_app.sh
@@ -3,6 +3,6 @@ chown -R app:app /home/app/oav_website
 cd /home/app/oav_website
 #rm -r /home/app/oav_website/vendor/bundle
 sudo -u app bundle install --deployment
-sudo -u app rake db:migrate RAILS_ENV=production
-sudo -u app rake assets:precompile
+sudo -u app bundle exec rake db:migrate RAILS_ENV=production
+sudo -u app bundle exec rake assets:precompile
 mkdir -p /var/log/nginx
