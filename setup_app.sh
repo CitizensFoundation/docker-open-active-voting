7803c994b114e9f591c691cb1ec85e595fba16fd
root <robert.bjarnason@gmail.com>
2018-03-01 17:34:59 +0000

    Ruby 2.9 upgrade

--- setup_app.sh
+++ setup_app.sh
@@ -2,11 +2,7 @@
 chown -R app:app /home/app/oav_website
 cd /home/app/oav_website
 
-ls -l /usr/local/rvm/rubies/ruby-2.2.5/lib/ruby/site_ruby/2.2.0/rubygems/ssl_certs
-sudo cp ./GlobalSignRootCA.pem /usr/local/rvm/rubies/ruby-2.2.5/lib/ruby/site_ruby/2.2.0/rubygems/ssl_certs/ 
-ls -l /usr/local/rvm/rubies/ruby-2.2.5/lib/ruby/site_ruby/2.2.0/rubygems/ssl_certs
-
-sudo -u app gem update --system 2.6.1
+sudo -u app gem update
 sudo -u app gem install bundler
 
 rm -r /home/app/oav_website/vendor/bundle
