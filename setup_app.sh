ec8ba82def7567379cae8abc0a2dab79de4d6a27
Robert Bjarnason <robert.bjarnason@gmail.com>
2023-08-02 09:39:23 +0000

    update

--- setup_app.sh
+++ setup_app.sh
@@ -6,8 +6,8 @@ cd /home/app/oav_website
 #sudo cp ./GlobalSignRootCA.pem /usr/local/rvm/rubies/ruby-2.2.5/lib/ruby/site_ruby/2.2.0/rubygems/ssl_certs/ 
 #ls -l /usr/local/rvm/rubies/ruby-2.2.5/lib/ruby/site_ruby/2.2.0/rubygems/ssl_certs
 
-sudo -u app gem update 
-sudo -u app gem install bundler
+#sudo -u app gem update 
+#sudo -u app gem install bundler
 
 #rm -r /home/app/oav_website/vendor/bundle
 
